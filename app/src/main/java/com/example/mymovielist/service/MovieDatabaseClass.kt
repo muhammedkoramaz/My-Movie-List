480c32992622d4f77b04f8713c7a87574e93a1b5
Muhammed Koramaz <33615438+muhammedkoramaz@users.noreply.github.com>
2022-03-04 03:08:28 +0300

    data class created for sqlite.

--- app/src/main/java/com/example/mymovielist/service/MovieDatabaseClass.kt
+++ app/src/main/java/com/example/mymovielist/service/MovieDatabaseClass.kt
@@ -1,3 +1,16 @@
 package com.example.mymovielist.service
 
-data class MovieDatabaseClass()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movie")
+data class MovieDatabaseClass(
+    @ColumnInfo(name = "name")
+    var name: String,
+    @ColumnInfo(name = "image_url")
+    var image_url: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var dataId: Int = 0
+}
